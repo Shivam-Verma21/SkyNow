bd9bf90981d238f9dad978b6387136a71b99df1a
Shivam Verma <shivamverma21201@gmail.com>
2025-03-30 22:39:05 +0530

    Add CitySearch and ThemeToggle components to header

---
+++ src/components/CitySearch.tsx
@@ -0,0 +1,162 @@
+import {
+  // Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  // CommandShortcut,
+} from "@/components/ui/command";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { Clock, Loader2, Search, XCircle } from "lucide-react";
+import React from "react";
+import { useLocationSearch } from "@/hooks/use-weather";
+import { useNavigate } from "react-router";
+import { useSearchHistory } from "@/hooks/use-searchHistory";
+import { format } from "date-fns";
+
+const CitySearch = () => {
+  const [open, setOpen] = useState(false);
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const { data: locations, isLoading } = useLocationSearch(query);
+
+  const { history, addToHistory, clearHistory } = useSearchHistory();
+
+  const handleSelect = (cityData: string) => {
+    const [lat, lon, name, country] = cityData.split("|");
+
+    addToHistory.mutate({
+      query,
+      name,
+      lat: parseFloat(lat),
+      lon: parseFloat(lon),
+      country,
+    });
+
+    navigate(`/city/${name}??lat=${lat}&lon=${lon}`);
+    setOpen(false);
+    setQuery("");
+  };
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant={"outline"}
+        className="cursor-pointer relative justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"
+        onClick={() => setOpen(true)}
+      >
+        <Search className="mr-2 h-4 w-4" />
+        Search Cities...
+      </Button>
+
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput
+          placeholder="Search Cities..."
+          value={query}
+          onValueChange={setQuery}
+        />
+        <CommandList>
+          {query.length > 2 && !isLoading && (
+            <CommandEmpty>No results found.</CommandEmpty>
+          )}
+
+          {/* <CommandGroup heading="Favorites">
+            <CommandItem>Calendar</CommandItem>
+          </CommandGroup> */}
+
+          {history.length > 0 && (
+            <>
+              <CommandSeparator />
+              <CommandGroup heading="Recent Searches">
+                <div>
+                  <p>Recent Searches</p>
+                  <Button
+                    variant={"ghost"}
+                    size={"sm"}
+                    onClick={() => clearHistory.mutate()}
+                  >
+                    <XCircle className="h-4 w-4" />
+                    Clear
+                  </Button>
+                </div>
+                {history.map((location) => {
+                  return (
+                    <CommandItem
+                      key={`${location.lat}-${location.lon}`}
+                      value={`${location.lat} | ${location.lon}|${location.name}|${location.country}`}
+                      onSelect={handleSelect}
+                    >
+                      <Clock className="mr-2 h-4 w-4 text-muted-foreground" />
+                      <span>{location.name}</span>
+                      {location.state && (
+                        <span className="text-sm text-muted-foreground">
+                          , {location.state}
+                        </span>
+                      )}
+                      <span className="text-sm text-muted-foreground">
+                        , {location.country}
+                      </span>
+                      <span className="ml-auto text-xs text-muted-foreground">
+                        {format(location.searchedAt, "MMM d, h:mm a")}
+                      </span>
+                    </CommandItem>
+                  );
+                })}
+              </CommandGroup>
+            </>
+          )}
+
+          <CommandSeparator />
+
+          {locations && locations.length > 0 && (
+            <CommandGroup heading="Suggestions">
+              {isLoading && (
+                <div className="flex items-center justify-center p-4">
+                  <Loader2 className="h-4 w-4 animate-spin" />
+                </div>
+              )}
+              {locations.map((location) => {
+                return (
+                  <CommandItem
+                    key={`${location.lat}-${location.lon}`}
+                    value={`${location.lat} | ${location.lon}|${location.name}|${location.country}`}
+                    onSelect={handleSelect}
+                  >
+                    <Search className="mr-2 h-4 w-4" />
+                    <span>{location.name}</span>
+                    {location.state && (
+                      <span className="text-sm text-muted-foreground">
+                        , {location.state}
+                      </span>
+                    )}
+                    <span className="text-sm text-muted-foreground">
+                      , {location.country}
+                    </span>
+                  </CommandItem>
+                );
+              })}
+            </CommandGroup>
+          )}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+
+export default CitySearch;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,9 @@
 // import React from "react";
 import { Link } from "react-router";
-import { useTheme } from "@/context/theme-provider";
-import { Github, Moon, Sun } from "lucide-react";
+import CitySearch from "./CitySearch";
+import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
-  const isDark = theme === "dark";
-
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-3 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-20 items-center justify-between px-4">
@@ -14,30 +11,22 @@ const Header = () => {
           <img src="/logo.png" alt="SkyNow" className="h-20 rounded-full" />
         </Link>
 
-        <div className="flex gap-7">
-          {/* //todo search input */}
+        <div className="flex gap-4 items-center">
+          <CitySearch />
 
-          <div title="Go to SkyNow github" className="h-6 w-6 hover:scale-120 ease-in-out duration-200">
+          {/* <div
+            title="Go to SkyNow github"
+            className="h-6 w-6 hover:scale-120 ease-in-out duration-200"
+          >
             <Link
               to={"https://github.com/Shivam-Verma21/SkyNow"}
               target="_blank"
             >
               <Github />
             </Link>
-          </div>
+          </div> */}
 
-          <div
-            title="Toggle Theme"
-            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-            className={`flex items-center cursor-pointer transition-transform duration-500 
-              ${isDark ? "rotate-180" : "rotate-0"}`}
-          >
-            {isDark ? (
-              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all" />
-            ) : (
-              <Moon className="h-6 w-6 text-blue-500 rotate-0 transition-all" />
-            )}
-          </div>
+          <ThemeToggle />
         </div>
       </div>
     </header>
