e8a04230f6fc3ccd1aa59c416bcd9ffb2a29613d
Shivam Verma <shivamverma21201@gmail.com>
2025-03-30 23:29:40 +0530

    Add dark and light logo images and update Header component to use theme-based logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,21 @@
 import { Link } from "react-router";
 import CitySearch from "./CitySearch";
 import ThemeToggle from "./ThemeToggle";
+import { useTheme } from "@/context/theme-provider";
 
 const Header = () => {
+  const { theme } = useTheme();
+
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-3 supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-20 items-center justify-between px-4">
         <Link to={"/"}>
-          <img src="/logo.png" alt="SkyNow" className="h-20 rounded-full" />
+          {/* <img src="/logo.png" alt="SkyNow" className="h-20 rounded-full" /> */}
+          <img
+            src={theme === "dark" ? "/dark-logo.png" : "/light-logo.png"}
+            alt="SkyNow Logo"
+            className="h-20 rounded-full"
+          />
         </Link>
 
         <div className="flex gap-4 items-center">
